96e5c96ddd98c726abdcb74c45a1aef402ba1fc3
cosmicARTS <cosmicartz@gmail.com>
2014-08-26 16:42:33 -0600

    Initial Commit

---
+++ src/me/cosmicarts/MobGUI/Main.java
@@ -0,0 +1,99 @@
+package me.cosmicarts.MobGUI;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.ssmc.customentitiesapi.entities.CustomEntities;
+import net.ssmc.customentitiesapi.entities.CustomEntityZombie;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Zombie;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+
+	public Inventory inv;
+
+	public ItemStack sg = new ItemStack(Material.ENDER_PEARL, 1);
+	{
+		ItemMeta sgmeta = sg.getItemMeta();
+		sgmeta.setDisplayName(ChatColor.RED + "Survival Games");
+		List<String> sglore = new ArrayList<String>();
+		sglore.add(ChatColor.YELLOW
+				+ "Click to join the Survival Games server!");
+		sgmeta.setLore(sglore);
+		sg.setItemMeta(sgmeta);
+	}
+
+	public void onEnable() {
+
+		PluginManager pm = Bukkit.getPluginManager();
+		pm.registerEvents(this, this);
+
+	}
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args) {
+
+		if (cmd.getName().equalsIgnoreCase("mobgui")
+				&& sender instanceof Player) {
+
+			CustomEntityZombie cez = CustomEntities
+					.getNewCustomEntityZombie(((Player) sender).getLocation());
+			cez.removeGoalSelectorPathfinderGoalAll();
+			cez.setCustomName(ChatColor.YELLOW + "Server Selector");
+			cez.setPushable(false);
+
+		}
+
+		return false;
+
+	}
+
+	@EventHandler
+	public void onSkeletonClick(PlayerInteractEntityEvent event) {
+
+		if (event.getRightClicked() instanceof Zombie) {
+			Player player = event.getPlayer();
+
+			inv = Bukkit.createInventory(null, 54, ChatColor.GRAY
+					+ "          Server Selector");
+
+			inv.setItem(31, sg);
+			inv.setItem(22, new ItemStack(Material.FIREBALL));
+
+			player.openInventory(inv);
+		}
+	}
+
+	@EventHandler
+	public void onInvClick(InventoryClickEvent event) {
+
+		Player player = (Player) event.getWhoClicked();
+
+		if (!player.isOp()) {
+
+			event.setCancelled(true);
+
+		} else {
+
+			return;
+
+		}
+
+	}
+
+}
