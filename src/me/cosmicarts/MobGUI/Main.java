d9bbfb7fd6b9bdb7933aaf3cd9b0fae7eb96ee5b
Cameron <cosmicartz@gmail.com>
2014-08-26 20:55:12 -0600

    Command bug fix

--- src/me/cosmicarts/MobGUI/Main.java
+++ src/me/cosmicarts/MobGUI/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import net.ssmc.customentitiesapi.entities.CustomEntities;
-import net.ssmc.customentitiesapi.entities.CustomEntityZombie;
+import net.ssmc.customentitiesapi.entities.CustomEntitySheep;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -12,7 +12,7 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Zombie;
+import org.bukkit.entity.Sheep;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -48,14 +48,46 @@ public class Main extends JavaPlugin implements Listener {
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args) {
 
-		if (cmd.getName().equalsIgnoreCase("mobgui")
+		Player player = (Player) sender;
+
+		if (cmd.getName().equalsIgnoreCase("mg")
+				|| cmd.getName().equalsIgnoreCase("mobgui")
 				&& sender instanceof Player) {
 
-			CustomEntityZombie cez = CustomEntities
-					.getNewCustomEntityZombie(((Player) sender).getLocation());
-			cez.removeGoalSelectorPathfinderGoalAll();
-			cez.setCustomName(ChatColor.YELLOW + "Server Selector");
-			cez.setPushable(false);
+			if (args.length == 1) {
+
+				if (args[0].equalsIgnoreCase("spawn")) {
+
+					CustomEntitySheep ces = CustomEntities
+							.getNewCustomEntitySheep(((Player) sender)
+									.getLocation());
+					ces.removeGoalSelectorPathfinderGoalAll();
+					ces.setCustomName(ChatColor.YELLOW + "Server Selector");
+					ces.setCustomNameVisible(true);
+					ces.setUnpushable();
+					ces.setUnableToMove();
+
+				}
+
+				else if (args[0].equalsIgnoreCase("about")) {
+
+					player.sendMessage(ChatColor.GOLD + "" + ChatColor.ITALIC
+							+ "MobGUI " + ChatColor.DARK_GRAY + "• "
+							+ ChatColor.AQUA
+							+ "MobGUI was developed in full by PistonGaming.");
+
+				}
+
+			}
+
+			else {
+
+				player.sendMessage(ChatColor.GOLD + "" + ChatColor.ITALIC
+						+ "MobGUI " + ChatColor.DARK_GRAY + "• "
+						+ ChatColor.RED + "Incorrect Usage.\n" + ChatColor.RED
+						+ "/mg <about/spawn>");
+
+			}
 
 		}
 
@@ -66,14 +98,14 @@ public class Main extends JavaPlugin implements Listener {
 	@EventHandler
 	public void onSkeletonClick(PlayerInteractEntityEvent event) {
 
-		if (event.getRightClicked() instanceof Zombie) {
+		if (event.getRightClicked() instanceof Sheep) {
 			Player player = event.getPlayer();
 
 			inv = Bukkit.createInventory(null, 54, ChatColor.GRAY
 					+ "          Server Selector");
 
 			inv.setItem(31, sg);
-			inv.setItem(22, new ItemStack(Material.FIREBALL));
+			inv.setItem(22, sg);
 
 			player.openInventory(inv);
 		}
